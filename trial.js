d3d43360bcea96b4637fa7236509bc5b1681461f
ompurwar <kaidranzerom@gmail.com>
2017-12-13 02:34:07 +0530

    commit

--- trial.js
+++ trial.js
@@ -1,12 +1,12 @@
-var mongoClient = require('mongodb').MongoClient
-var url = "mongodb://localhost:27017/mynode-app"        //database name: mynode-app
+var mongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mynode-app";        //database name: mynode-app
 
 mongoClient.connect(url, function (err, db) {
   if (err) {                                              // throwing the err
-    console.log("Sorry Couldn't connect! ('_') \nSome error occured")
-    throw err
+    console.log("Sorry Couldn't connect! ('_') \nSome error occured");
+    throw err;
   }
 
   console.log("connected to mongodb: mynode-app")
-  db.close(function () { console.log('connection terminated') })
-})
+  db.close(function () { console.log('connection terminated'); });
+});
