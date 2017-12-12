bc2ac7aea7462df0e367056eb4112a9a1c8533bd
Om Purwar <kaidranzerom@gmail.com>
2017-12-12 20:58:23 +0530

    Add files via upload

---
+++ trial.js
@@ -0,0 +1,15 @@
+const stitch = require("mongodb-stitch")
+const client = new stitch.StitchClient('trial-awkdu');
+const db = client.service('mongodb', 'mongodb-atlas').db('my-first-trial');
+client.login().then(() =>
+  db.collection('trial').updateOne({ owner_id: client.authedId() }, { $set: { number: 42 } }, { upsert: true })
+).then(() =>
+  db.collection('trial').find({ owner_id: client.authedId() }).limit(100).execute()
+  ).then(docs => {
+    console.log("Found docs", docs)
+    //console.log("[MongoDB Stitch] Connected to Stitch")
+  }).catch(err => {
+    console.error(err)
+  });
+
+  console.log("hello om")
