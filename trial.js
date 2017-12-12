aa8f525f4f452073262155152cab558905e3f58f
ompurwar <kaidranzerom@gmail.com>
2017-12-13 03:19:31 +0530

    creating collection

--- trial.js
+++ trial.js
@@ -1,20 +1,23 @@
 var mongoClient = require('mongodb').MongoClient
-var url = "mongodb://localhost:27017/mynode-app"      //database name: mynode-app
+var url = "mongodb://localhost:27017/mynode-app" //database name: mynode-app
 
-mongoClient.connect(url, function (err, db) {
-  if (err) {                                              // throwing the err
+mongoClient.connect(url, function (err, client) {
+  if (err) { // throwing the err
     console.log("Sorry Couldn't connect! ('_') \nSome error occured")
     throw err
   }
-  db.createCollection("user", function(err ,res){
-    if(err){
+  const db = client.db("mynode-app")
+  db.createCollection("user", function (err, res) {
+    if (err) {
       console.log('some error occured')
       throw err;
-    }else{
+    } else {
       console.log("success!")
     }
+    client.close(function () {
+      console.log('connection terminated')
+    })
   })
 
-  console.log("connected to mongodb: mynode-app")
-  db.close(function () { console.log('connection terminated') })
+
 })
