ca4ef37b39e999cd146cf2c910c1e2e420c4801f
ompurwar <kaidranzerom@gmail.com>
2017-12-13 16:20:53 +0530

    noe you can do find also

--- trial.js
+++ trial.js
@@ -5,29 +5,47 @@ mongoClient.connect(url, function (err, client) {
   if (err) { // throwing the err
     console.log("Sorry Couldn't connect! ('_') \nSome error occured")
     throw err
-  }
-  const db = client.db("mynode-app")            //Declaring a db object
-  db.createCollection("user", function (err, res) {
-    if (err) {                                  //Checking for the error while creating collection
-      console.log('some error occured')
-      throw err;                                //Throwing if any exixts
-    } else {
-      console.log("success!")
-      var myobj = {
-        "name": {                               // Creating an object to insert
-          "first-name": "Om",
-          "last-name": "Purwar"
+  } else {
+    const db = client.db("mynode-app") //Declaring a db object
+    db.createCollection("user", function (err, res) {
+      if (err) { //Checking for the error while creating collection
+        console.log('some error occured')
+        throw err; //Throwing if any exixts
+      } else {
+
+        console.log("success!")
+
+        var myobj = {
+          "name": { // Creating an object to insert
+            "first-name": "Om",
+            "last-name": "Purwar"
+          }
         }
+
+        db.collection("user").insertOne(myobj, function (err, res) { //Inserting an objet to collection
+
+          if (err) {
+            throw err
+          } else {
+
+            console.log("object insearted")
+
+            db.collection("user").findOne({}, function (err, result) {
+
+              if (err) {
+                throw err;
+              } else {
+                console.log(JSON.stringify(result.name['first-name']))
+              }
+
+            })
+
+            client.close(function () {
+              console.log('connection terminated')
+            })
+          }
+        })
       }
-      db.collection("user").insertOne(myobj, function (err, res) { //Inserting an objet to collection
-        if (err) throw err
-        else {
-          console.log("object insearted")
-          client.close(function () {
-            console.log('connection terminated')
-          })
-        }
-      })
-    }
-  })
+    })
+  }
 })
