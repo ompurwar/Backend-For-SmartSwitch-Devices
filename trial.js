74fecfaecabe39c196bf973f12796444993f1dfc
ompurwar <kaidranzerom@gmail.com>
2018-02-26 14:05:27 +0530

    logging with Morgan

--- trial.js
+++ trial.js
@@ -3,14 +3,15 @@ const mongoClient = require('mongodb').MongoClient;
 // mynode-app
 const pass = 'Ee332MEd8cJBVDpX';
 const url = 'mongodb+srv://turbo:' + pass +
-  '@cluster0-ztclp.mongodb.net'; // database name: SwitchGrid
+    '@cluster0-ztclp.mongodb.net';  // database name: SwitchGrid
 
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
 
 // setting up the server
-const server = app.listen(8080, function () {
+const server = app.listen(8080, function() {
   console.log('listening to the port\t:' + server.address().port);
 });
 
@@ -18,20 +19,24 @@ const server = app.listen(8080, function () {
 app.use(bodyParser.json());
 
 // setting header
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
+app.use(
+    morgan('combined'));  // using morgan a as middleware for logging purposes
 
 // sending UI page
 app.get(
     '', function(req, res) { res.sendFile(__dirname + '/login/index.html'); });
 app.get(
     '/', function(req, res) { res.sendFile(__dirname + '/login/index.html'); });
+
 app.get('/js/index.js', function(req, res) {
   console.log('\n \t:/js/index.js\t file requested!');
   res.sendFile(__dirname + '/login/js/index.js');
 });
+
 app.get('/css/style.css', function(req, res) {
   console.log('\n \t:/css/style.css\t file requested!');
   res.sendFile(__dirname + '/login/css/style.css');
@@ -43,10 +48,7 @@ const InsertAndFindData = (req, res, next) => {
   } else {
     var myobj = req.body;
     // connecting to the server
-    mongoClient.connect(url, {
-      poolSize: 10,
-      ssl: true
-    }, function (err, client) {
+    mongoClient.connect(url, {poolSize: 10, ssl: true}, function(err, client) {
       if (err) {
         // throwing the err
         console.log('Sorry Couldn\'t connect! (\'_\') \nSome error occured');
@@ -56,16 +58,14 @@ const InsertAndFindData = (req, res, next) => {
         // Declaring a db object
         var db = client.db('SwitchGrid');
         // Inserting an objet to collection
-        db.collection('user').insertOne(myobj, function (err) {
+        db.collection('user').insertOne(myobj, function(err) {
           if (err) {
             console.log('error on line:\t44', err);
             res.json(err);
           } else {
             console.log('object insearted');
             res.json('object insearted:\t' + JSON.stringify(myobj));
-            client.close(function () {
-              console.log('connection terminated');
-            });
+            client.close(function() { console.log('connection terminated'); });
           }
         });
       }
@@ -76,10 +76,7 @@ const InsertAndFindData = (req, res, next) => {
 
 const FindData = (req, res, next) => {
   const myobj = req.body;
-  mongoClient.connect(url, {
-    poolSize: 10,
-    ssl: true
-  }, function (err, client) {
+  mongoClient.connect(url, {poolSize: 10, ssl: true}, function(err, client) {
     if (err) {
       // throwing the err
       console.log('Sorry Couldn\'t connect! (\'_\') \nSome error occured');
@@ -88,7 +85,7 @@ const FindData = (req, res, next) => {
       console.log('Connected correctly to server');
       // Declaring a db object
       var db = client.db('SwitchGrid');
-      db.collection('user').find(myobj).toArray(function (err, result) {
+      db.collection('user').find(myobj).toArray(function(err, result) {
 
         if (err) {
           throw err;
@@ -96,21 +93,16 @@ const FindData = (req, res, next) => {
           console.log(JSON.stringify(result));
           res.json(result);
         }
-        client.close(function () {
-          console.log('connection terminated');
-        });
+        client.close(function() { console.log('connection terminated'); });
       });
     }
   });
   next();
 };
 
-//middleware for dele
+// middleware for dele
 const DeleteData = (req, res, next) => {
-  mongoClient.connect(url, {
-    poolSize: 10,
-    ssl: true
-  }, function (err, client) {
+  mongoClient.connect(url, {poolSize: 10, ssl: true}, function(err, client) {
     if (err) {
       // throwing the err
       console.log('Sorry Couldn\'t connect! (\'_\') \nSome error occured');
@@ -120,14 +112,12 @@ const DeleteData = (req, res, next) => {
       // Declaring a db object
       var db = client.db('SwitchGrid');
       var myquery = req.body;
-      db.collection('user').deleteOne(myquery, function (err, obj) {
+      db.collection('user').deleteOne(myquery, function(err, obj) {
         // throwing the err
         if (err) throw err;
-        console.log("deleted:\t " + obj.deletedCount);
-        client.close(function () {
-          console.log('connection terminated');
-        });
-        res.json("deleted:\t " + obj.deletedCount);
+        console.log('deleted:\t ' + obj.deletedCount);
+        client.close(function() { console.log('connection terminated'); });
+        res.json('deleted:\t ' + obj.deletedCount);
       });
     }
   });
@@ -137,14 +127,14 @@ const DeleteData = (req, res, next) => {
 
 /* call with:
 {
-	"Human":{
-		"name":"Om Purwar1",
-		"age":36,
-		"DOB":"12/06/1998"
-	}
+        "Human":{
+                "name":"Om Purwar1",
+                "age":36,
+                "DOB":"12/06/1998"
+        }
 }*/
 app.use('/api/addData', InsertAndFindData);
-app.post('/api/addData', function (req, res) {
+app.post('/api/addData', function(req, res) {
   console.log('wellcome! to mongoDb app \t/api/addData');
   console.log(req.body);
 });
@@ -152,14 +142,15 @@ app.post('/api/addData', function (req, res) {
 // call with  {"Human.name":"Om Purwar singh"}
 // call with  {"Human.age":"23"}
 app.use('/api/findData', FindData);
-app.post('/api/findData', function (req, res) {
+app.post('/api/findData', function(req, res) {
   console.log('wellcome! to mongoDb app \t/api/findData');
   console.log(req.body);
 });
 
 // call with  {"Human.name":"Om Purwar singh"}
 app.use('/api/deleteData', DeleteData);
-app.post('/api/deleteData', function (req, res) {
+app.post('/api/deleteData', function(req, res) {
   console.log('wellcome! to mongoDb app \t/api/deleteData');
   console.log(req.body);
 });
+
